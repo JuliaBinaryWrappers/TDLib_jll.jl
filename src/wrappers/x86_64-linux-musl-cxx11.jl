385e6f3413e1392a6bc62048ef43dae11e911e78
jlbuild <juliabuildbot@gmail.com>
2021-06-08 13:13:55 +0000

    TDLib_jll build 1.7.4+0

--- src/wrappers/x86_64-linux-musl-cxx11.jl
+++ src/wrappers/x86_64-linux-musl-cxx11.jl
@@ -4,7 +4,7 @@ export libtdjson
 using OpenSSL_jll
 using Zlib_jll
 JLLWrappers.@generate_wrapper_header("TDLib")
-JLLWrappers.@declare_library_product(libtdjson, "libtdjson.so.1.6.10")
+JLLWrappers.@declare_library_product(libtdjson, "libtdjson.so.1.7.4")
 function __init__()
     JLLWrappers.@generate_init_header(OpenSSL_jll, Zlib_jll)
     JLLWrappers.@init_library_product(
